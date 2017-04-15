7f070e1e6cd82f0fc97fe0300dc384d116ea196c
Marcus <mattosse@hotmail.com>
2017-04-15 19:24:14 +0200

    up
    up

--- www/audiotoggle.js
+++ www/audiotoggle.js
@@ -4,6 +4,7 @@ exports.SPEAKER = 'speaker';
 exports.EARPIECE = 'earpiece';
 exports.NORMAL = 'normal';
 exports.RINGTONE = 'ringtone';
+exports.TEST = 'HELLO';
 
 exports.setAudioMode = function (mode) {
 	cordova.exec(null, null, 'AudioTogglePlugin', 'setAudioMode', [mode]);
